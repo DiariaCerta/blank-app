5b581b8fc6d4328a45321aec024c4783593494ab
DiariaCerta <diariacert@gmail.com>
2025-04-07 23:55:22 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,9 @@
 import streamlit as st
 
 st.title("🎈 Analiador de CNPJ MEI")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+
+pip install streamlit requests beautifulsoup4
+
 import streamlit as st
 import requests
 from bs4 import BeautifulSoup
